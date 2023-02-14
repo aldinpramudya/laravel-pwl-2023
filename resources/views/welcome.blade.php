f4b6d3ce9faf5f12af3a8557b64a7274cc1cad4c
aldinpramudya <aldinarielpramudya@gmail.com>
2023-02-14 15:23:55 +0700

    change nama

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -22,8 +22,8 @@
     </head>
     <body class="antialiased">
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
-           <div class="ml-4 text-lg leading-7 font-semibold"><h1 class="text-gray-900 dark:text-white">Aldin Ariel Pramudya</h1>
-           </div>
+           <div class="ml-4 text-lg leading-7 font-semibold"><h1 class="text-gray-900 dark:text-white">Aldin Bin Halah</h1>
+           </div>   
         </div>
     </body>
 </html>
